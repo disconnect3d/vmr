34cb0342874542ef880c2f0aababd240991260cf
disconnect3d <dominik.b.czarnota@gmail.com>
2019-11-24 13:51:19 +0100

    Improve setup a bit

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import setup, find_packages
 
+long_description = '''
+vmware-cli is a minimal wrapper for vmware's vmrun command.
+
+It was created because I felt that `vmrun` CLI was not so convenient.
+'''
+
 setup(
     name='vmr',
-    description='vmr',
+    description='vmr - a better vmware cli (vmrun)',
+    long_description=long_description,
     url='https://github.com/disconnect3d/vmr',
     author='disconnect3d',
+    author_email='dominik.b.czarnota+vmr@gmail.com',
+    keywords='vmr vmrun vmware cli',
     version='0.1.0',
     packages=find_packages(),
     python_requires='>=3.7',
